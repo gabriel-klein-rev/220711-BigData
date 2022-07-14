5c206368d9c7cabe92ebc8cbd0a05ea518287739
Gabriel Klein <87140630+GabrielMichaelKlein@users.noreply.github.com>
2022-07-14 09:57:18 -0700

    Updated week1

--- week1/Animal_Journal.py
+++ week1/Animal_Journal.py
@@ -1,16 +1,135 @@
-from Animals import Animal, Dog, Cat
+from Animals import Animal, Dog, Cat, Horse
 
-d1 = Dog("Fido", 10, "brown")
-c1 = Cat("Garfield", 15, "orange")
+# # a1 = Animal("fred", 13, "blue")
+# # a1.move()
 
-lst_Animals = [d1, c1]
+# d1 = Dog("Fido", 10, "brown")
+# c1 = Cat("Garfield", 15, "orange")
 
-for animal in lst_Animals:
-    animal.sleep()
-    animal.move()
-    animal.getOlder(3)
-    print(animal)
+# lst_Animals = [d1, c1]
 
-# c1._name = "asdf"
-c1.setName(5)
-print(c1)
+# for animal in lst_Animals:
+#     animal.sleep()
+#     animal.move()
+#     animal.getOlder(3)
+#     print(animal)
+
+# # c1._name = "asdf"
+# c1.setName(5)
+# print(c1)
+
+# TODO: Exception handling
+# Stretch goal: Be able to modify animals, call methods
+
+
+def main():
+
+    print("*** WELCOME TO THE ANIMAL JOURNAL! ***")
+
+    fname = "all_animals.csv"
+    lst_animals = []
+    lst_animals = load_animals(fname)
+
+    while(True):
+        animal = insert_animal()
+        if animal == None:
+            break
+        
+        lst_animals.append(animal)
+    
+    print("\n\n***** All animals *****")
+    for elem in lst_animals:
+        print(elem)
+
+    save_animals(fname, lst_animals)
+    print("\n\n")
+
+def save_animals(fname, lst_animals):
+    '''
+    save_animals
+
+    This function will take in a list of animals and save them to a csv file.
+
+    Returns None
+    '''
+    with open(fname, "w") as f:
+        for animal in lst_animals:
+            if type(animal) == Cat:
+                f.write("Cat," + animal._name + "," + str(animal._age) + "," + animal._color + ",\n")
+            elif type(animal) == Dog:
+                f.write("Dog," + animal._name + "," + str(animal._age) + "," + animal._color + ",\n")
+            elif type(animal) == Horse:
+                f.write("Horse," + animal._name + "," + str(animal._age) + "," + animal._color + ",\n")
+            else:
+                pass
+
+
+
+def load_animals(fname) -> list:
+    '''
+    load_animals
+
+    This function will take in a file name representing a csv file of animals to 
+    load into an animal list.
+
+    Returns list of Animals
+    '''
+    lst_animals = []
+
+    with open(fname, "r") as f:
+        for line in f:
+            info = line.split(',')
+            if info[0] == "Cat":
+                animal = Cat(info[1], info[2], info[3])
+            elif info[0] == "Dog":
+                animal = Dog(info[1], info[2], info[3])
+            elif info[0] == "Horse":
+                animal = Horse(info[1], info[2], info[3])
+            else:
+                animal = None
+
+            if animal != None:
+                lst_animals.append(animal)
+
+    return lst_animals
+
+    
+def insert_animal() -> Animal:
+    '''
+    insert_animal
+
+    This function prompts the user for information about what animal 
+    to insert into the journal
+
+    Returns Animal
+    Returns None if user wants to quit
+    '''
+    print("\nPlease select which animal you want to enter into the journal:")
+    print("\t1) Cat")
+    print("\t2) Dog")
+    print("\t3) Horse")
+    print("\t4) quit")
+    animal_type = input(">>> ")
+
+    # Break out of function if user wants to quit
+    if animal_type == "4":
+        return None
+
+    name = input("\nEnter animal's name:\n>>>")
+    age = int(input("\nEnter animal's age:\n>>>"))
+    color = input("\nEnter animal's color:\n>>>")
+
+    if animal_type == "1":
+        animal = Cat(name, age, color)
+    elif animal_type == "2":
+        animal = Dog(name, age, color)
+    elif animal_type == "3":
+        animal = Horse(name, age, color)
+    else:
+        animal = None
+
+    return animal
+
+
+if __name__ == "__main__":
+    main()
--- week1/Animals.py
+++ week1/Animals.py
@@ -16,10 +16,18 @@ OOP
         - *** CAN"T MAKE ATTRIBUTES PRIVATE IN PYTHON ***
             - "Emulate" by prefixing attributes with _
         - Access attributes with control using setter/getter methods
+- Abstraction
+    - Handling complexity byu hiding unnecessary information from the user
+    - Achieved by using abstract classes
+        - Class that has one or more abstract methods
+            - Abstract method => unimplemented method
+    - abc (Abstract Base Class) module used to create abstract classes
 
 '''
 
-class Animal:
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
     def __init__(self, name, age, color=""):
         self._name = str(name)
         self._age = int(age)
@@ -31,20 +39,16 @@ class Animal:
     def getName(self):
         return self._name
 
-    # def getPhoneNumber(self):
-    #     return "xxx-xxx-" + _phoneNumber[-4:]
-
+    @abstractmethod
     def sleep(self):
-        print("***zzz***")
+        pass
 
     def getOlder(self, years=1):
         self._age += years
-    
-    # def getOlder(self):
-    #     self.age += 1
 
+    @abstractmethod
     def move(self):
-        print("*", self._name, "has moved. *")
+        pass
 
     def __str__(self):
         return "Name: " + self._name + ", Age: " + str(self._age) + ", Color: " + self._color
@@ -62,5 +66,17 @@ class Cat(Animal):
 
     def move(self):
         print("*", self._name, "walked. *")
+
+class Horse(Animal):
+    def sleep(self):
+        print("***sleep***")
+    
+    def move(self):
+        print("*", self.name, "galloped *")
+
+    def feed(self):
+        print("** Fed", self.name, "a carrot. **")
+    
+
 
 
